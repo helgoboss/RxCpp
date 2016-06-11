3b55e661cf0601bd5456b3225bdd8c757ebee45d
Benjamin Klum <bklum@avantgarde-labs.de>
2016-06-11 17:21:22 +0200

    Don't complain if queue not empty on destruction

--- Rx/v2/src/rxcpp/schedulers/rx-runloop.hpp
+++ Rx/v2/src/rxcpp/schedulers/rx-runloop.hpp
@@ -147,7 +147,7 @@ public:
 //        queue_type::destroy();
 
         auto expired = std::move(state->q);
-        if (!state->q.empty()) abort();
+        // if (!state->q.empty()) abort();
     }
 
     clock_type::time_point now() const {
